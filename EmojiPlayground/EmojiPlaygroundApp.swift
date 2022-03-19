4be8f2862a86244af4b624d79c7237c68920c47e
Changsu Lee <729mail2@gmail.com>
2022-03-19 16:18:49 +0900

    Initial Commit

---
+++ EmojiPlayground/EmojiPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPlaygroundApp.swift
+//  EmojiPlayground
+//
+//  Created by Changsu Lee on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

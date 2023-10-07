6d15d7509b495429e40ea262b613c3e1786adfba
Changsu Lee <729mail2@gmail.com>
2023-10-07 18:44:55 +0900

    firebase 추가

--- EmojiPlayground/Sources/App/EmojiPlaygroundApp.swift
+++ EmojiPlayground/Sources/App/EmojiPlaygroundApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseRemoteConfig
 
 @main
 struct EmojiPlaygroundApp: App {
@@ -15,41 +17,90 @@ struct EmojiPlaygroundApp: App {
     @StateObject var emoticonStorage = EmoticonStorage()
     
     @State private var iCloudAccountNotFoundAlert = false
+    @State private var isSuccessedVersionCheck: Bool?
+    
+    init() {
+        FirebaseApp.configure()
+        
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        RemoteConfig.remoteConfig().configSettings = settings
+    }
     
     var body: some Scene {
         WindowGroup {
-            MainView()
-                .environment(\.managedObjectContext, PersistenceController.shared.context)
-                .environmentObject(mainRouter)
-                .environmentObject(emoticonStorage)
-                .environmentObject(Theme.shared)
-                .overlay(mainOverlay)
-                .alert("로그인 오류", isPresented: $iCloudAccountNotFoundAlert, actions: {
-                    Button("설정으로 이동") {
-                        UIApplication.shared.open(URL(string: UIApplication.openNotificationSettingsURLString)!)
+            if isSuccessedVersionCheck == true {
+                MainView()
+                    .environment(\.managedObjectContext, PersistenceController.shared.context)
+                    .environmentObject(mainRouter)
+                    .environmentObject(emoticonStorage)
+                    .environmentObject(Theme.shared)
+                    .overlay(mainOverlay)
+                    .alert("로그인 오류", isPresented: $iCloudAccountNotFoundAlert, actions: {
+                        Button("설정으로 이동") {
+                            UIApplication.shared.open(URL(string: UIApplication.openNotificationSettingsURLString)!)
+                        }
+                        
+                        Button("취소", role: .cancel) { }
+                    }, message: {
+                        Text("설정에서 iCloud에 로그인을 해주세요.")
+                    })
+                    .onAppear {
+                        checkiCloudLoggedIn()
                     }
-                    
-                    Button("취소", role: .cancel) { }
-                }, message: {
-                    Text("설정에서 iCloud에 로그인을 해주세요.")
-                })
-        }
-        .onChange(of: scenePhase) { newValue in
-            switch newValue {
-            case .active:
-                Task {
-                    do {
-                        let _ = try await CloudKitUtility.getiCloudStatus()
-                    } catch CloudKitUtility.CloudKitError.iCouldAccountNotFound {
-                        iCloudAccountNotFoundAlert = true
-                    } catch {
-                        print("cslog error: \(error)")
+                    .onChange(of: scenePhase) { newValue in
+                        if newValue == .active {
+                            checkiCloudLoggedIn()
+                        }
                     }
-                }
-            default:
-                break
+            } else {
+                Color(uiColor: .systemBackground)
+                    .alert("업데이트가 필요합니다.", presenting: $isSuccessedVersionCheck, actions: { _ in
+                        Button("업데이트") {
+                            UIApplication.shared.open(URL(string: UIApplication.openNotificationSettingsURLString)!)
+                        }
+                    }, message: { _ in
+                        Text("앱 업데이트 후 이용가능합니다.")
+                    })
+                    .onAppear {
+                        RemoteConfig.remoteConfig().fetch { (status, error) -> Void in
+                            if status == .success {
+                                RemoteConfig.remoteConfig().activate { changed, error in }
+                            } else {
+                                print("Error: \(error?.localizedDescription ?? "No error available.")")
+                            }
+                            checkMinVersion()
+                        }
+                    }
             }
         }
+    }
+    
+    private func checkiCloudLoggedIn() {
+        Task {
+            do {
+                let _ = try await CloudKitUtility.getiCloudStatus()
+            } catch CloudKitUtility.CloudKitError.iCouldAccountNotFound {
+                iCloudAccountNotFoundAlert = true
+            } catch CloudKitUtility.CloudKitError.iCouldAccountTemporarilyUnavailable {
+                iCloudAccountNotFoundAlert = true
+            } catch {
+                print("cslog error: \(error)")
+            }
+        }
+    }
+    
+    private func checkMinVersion() {
+        let minimumVersion = RemoteConfig.remoteConfig().configValue(forKey: "minimum_version")
+        guard
+            let minimumVersion = minimumVersion.stringValue,
+            let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        else {
+            isSuccessedVersionCheck = false
+            return
+        }
+        
+        isSuccessedVersionCheck = minimumVersion <= appVersion
     }
     
     @ViewBuilder
